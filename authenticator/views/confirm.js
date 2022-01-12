ebd75daf4defcd6f0ef2df8d3dc713474c980878
John <>
2022-01-12 15:24:21 -0500

    Basic implementation of confirm page

--- authenticator/views/confirm.js
+++ authenticator/views/confirm.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
+import IconButton from '../components/icon-button';
+import SaveIcon from '../assets/navigation_icons/save.png';
+
+// This is where the logic would go for taking the information object and storing it
+function save(information) {
+  console.log(information);
+}
 
 export default function Confirm(props) {
-  console.log(props.route.params);
+  const information = props.route.params;
 
   return (
-    <View>
-      <Text>Placeholder confirm screen</Text>
+    <View style={{ padding: 10 }}>
+      <Text>Application Name</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Application Name"
+        onChangeText={(text) => information.issuer = (text)}
+        defaultValue={information.issuer}
+      />
+      <Text>Username</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Usename"
+        onChangeText={(text) => information.account = (text)}
+        defaultValue={information.account}
+      />
+      <View style ={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+        <IconButton 
+          onPress={ () => {
+          save(information);
+          props.navigation.navigate('application-list', props); 
+        }}
+        img={SaveIcon}
+      />
+      </View>
     </View>
   );
 }
