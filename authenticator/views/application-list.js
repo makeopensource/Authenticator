446f6f8c2df52c8b2b886ef3838c18501871a58e
Matthew Rubino <mrrubino@buffalo.edu>
2022-03-28 17:02:24 -0400

    Load static data

--- authenticator/views/application-list.js
+++ authenticator/views/application-list.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
-import PropTypes from 'prop-types';
+import totp from 'totp-generator';
 import ApplicationListItem from '../components/application-list-item';
+import Text from '../components/styled-text';
 
-export default function ApplicationList({ data }) {
+// Simulates function that retrieves application objects from storage
+async function getAll() {
+  await new Promise((resolve) => { setTimeout(resolve, 2000); });
+
+  return [
+    {
+      name: 'Application',
+      username: 'name',
+      totp: '000000',
+      uri: 'https://reactnative.dev/img/tiny_logo.png',
+    },
+  ];
+}
+
+function getNewData(apps, period) {
+  return apps.map((app) => {
+    // const token = totp(app.secret, { period });
+    // const updated = { ...app };
+    // updated.totp = token;
+    const updated = app;
+    return updated;
+  });
+}
+
+export default function ApplicationList() {
+  const INTERVAL = 30;
+  const [data, setData] = useState(null);
+
+  // Loads data from storage and updates application list
+  useEffect(() => {
+    const fetchApps = async () => {
+      const apps = await getAll();
+      setData(apps);
+      setTimeout(() => setData(getNewData(data, INTERVAL)), INTERVAL);
+    };
+
+    fetchApps();
+  }, []);
+
+  // If data is being fetched from storage
+  if (!data) {
+    return (
+      <View style={{ flex: 1, margin: 30 }}>
+        <Text style={{ textAlign: 'center' }}>Loading applications...</Text>
+      </View>
+    );
+  }
+
+  // If there are no applications
+  if (!data.length) {
+    return (
+      <View style={{ flex: 1, margin: 30 }}>
+        <Text style={{ textAlign: 'center' }}>No applications found.</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <FlatList
@@ -14,12 +71,3 @@ export default function ApplicationList({ data }) {
     </View>
   );
 }
-
-ApplicationList.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    totp: PropTypes.string.isRequired,
-    uri: PropTypes.string.isRequired,
-  })).isRequired,
-};
