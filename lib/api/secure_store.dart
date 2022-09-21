9cd846a8264230557fe5af03f0b0be31f3170e35
Emil Kovacev <emilkova@buffalo.edu>
2022-09-21 18:20:07 -0400

    Custom hotp implementation

---
+++ lib/api/secure_store.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:authenticator/model/account.dart';
+import 'package:crypto/crypto.dart';
+
+class SecureStore {
+  final FlutterSecureStorage storage;
+
+  SecureStore(this.storage);
+
+  Future<List<Account>> getAccounts() async {
+    String accountsValue = (await storage.read(key: "accounts")) ?? "[]";
+    List<dynamic> accountsJson = jsonDecode(accountsValue);
+    List<Account> accounts =
+        List<Account>.from(accountsJson.map((json) => Account.fromJson(json)));
+
+    return accounts;
+  }
+
+  Future<void> addAccount(Account account) async {
+    List<Account> accounts = await getAccounts();
+
+    // Do not add account if ID already used
+    for (Account element in accounts) {
+      if (element.uuid == account.uuid) {
+        return;
+      }
+    }
+
+    accounts.add(account);
+    String accountsValue = jsonEncode(accounts);
+
+    await storage.write(key: "accounts", value: accountsValue);
+  }
+
+  Future<void> removeAccount(Account account) async {
+    List<Account> accounts = await getAccounts();
+    accounts.removeWhere((element) => element.uuid == account.uuid);
+    String accountsValue = jsonEncode(accounts);
+
+    await storage.write(key: "accounts", value: accountsValue);
+  }
+}
+
+List<int> hotp(String secret, int counter) {
+  List<int> secretBytes = utf8.encode(secret);
+  List<int> counterBytes = utf8.encode(counter.toString());
+  Hmac hmacSha1 = Hmac(sha256, secretBytes);
+  Digest digest = hmacSha1.convert(counterBytes);
+
+  return digest.bytes;
+}
