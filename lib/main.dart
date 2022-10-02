8b1bfcc0231f63b17cedc307993b8ca1f6e65247
alan soto <alansoto@buffalo.edu>
2022-10-01 20:58:47 -0400

    Added navigation, user guide, and settings page.
    And also adjusted main to navigate.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:authenticator/api/secure_store.dart';
+import 'package:authenticator/pages/settings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:authenticator/view/home.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: MyApp(),
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,18 +24,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      return Scaffold(
         body: Home(store: store),
         appBar: AppBar(
           title: const Text("Authenticator"),
           centerTitle: true,
-        ),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-    );
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(builder: (context) => const SettingsPage()));
+                },
+                icon: Image.asset("lib/assets/gear.png"),
+              padding: const EdgeInsets.only(right: 20.0),
+            )
+          ],
+        )
+      );
   }
 }
--- lib/pages/user_guide.dart
+++ lib/pages/user_guide.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 
-@override
-Widget build(BuildContext context){
-  return MaterialApp(
-      title: "User Guide",
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("User Guide"),
-            centerTitle: true,
+class UserGuide extends StatelessWidget {
+  const UserGuide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("User Guide"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              const Text("\nStep 1. Tap the add application button.\n",
+                  style: TextStyle(fontSize: 19)),
+              Image.asset("lib/assets/step1.png"), //Place holder image
+              const Text("\nStep 2. Scan the QR code with your camera.\n",
+                  style: TextStyle(fontSize: 19)),
+              Image.asset("lib/assets/step2.png"), //Place holder image
+              const Text("\nStep 3. Press the save button.\n",
+                  style: TextStyle(fontSize: 19)),
+              Image.asset("lib/assets/step3.png"), //Place holder image
+              const Text("\nStep 4. View your codes on the home screen.\n",
+                  style: TextStyle(fontSize: 19))
+            ],
           ),
-          body: Center(
-            child: ListView (
-              shrinkWrap: true,
-              children: <Widget> [
-                const Text("\nStep 1. Tap the add application button.\n", style: TextStyle(fontSize: 19)),
-                Image.asset("lib/assets/step1.png"), //Place holder image
-                const Text("\nStep 2. Scan the QR code with your camera.\n", style: TextStyle(fontSize: 19)),
-                Image.asset("lib/assets/step2.png"), //Place holder image
-                const Text("\nStep 3. Press the save button.\n", style: TextStyle(fontSize: 19)),
-                Image.asset("lib/assets/step3.png"), //Place holder image
-                const Text("\nStep 4. View your codes on the home screen.\n", style: TextStyle(fontSize: 19))
-              ],
-            ),
-          )
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      )
-  );
+        )
+    );
+  }
 }
